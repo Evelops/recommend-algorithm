81baf331c9060c90fc5311486dbcae856a8ed2dd
eomseung-yeol <co6747@naver.com>
2022-09-21 22:20:48 +0900

    feat: study with TF-IDF algorithm

--- Content-Based /TF-IDF.py
+++ Content-Based /TF-IDF.py
@@ -1,7 +1,7 @@
 import mlxtend
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer #
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
